275c98e04810faf6140dcc0dea3158d9237b7f8c
nandinic2 <2020nchakravorty@dwight.edu>
2019-10-10 15:03:11 -0400

    update

--- app/routes.py
+++ app/routes.py
@@ -23,69 +23,65 @@ mongo = PyMongo(app)
 Bootstrap(app)
 
 #the login form
-class LoginForm(FlaskForm):
-    #username part of login
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-    remember = BooleanField('remember me')
-
-class RegisterForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-
-class User(db.Model):
-    id = db.Column('id', db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
-    email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(80))
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    form = LoginForm()
-    #checks to see if form has been submitted
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            #if user logged in correctly, redirect to the dashboard
-            if user.password == form.password.data:
-                return redirect(url_for('dashboard'))
-        #if user doesn't exist or login incorrect
-        return '<h1> Invalid username or password </h1>'
-        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
-    return render_template('login.html', form = form)
-
-@app.route('/signup', methods=['POST', 'GET'])
-def signup():
-    form = RegisterForm()
-    #allow new user to be created
-    if form.validate_on_submit():
-        new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
-        db.session.add(new_user)
-        try:
-            db.session.commit()
-            return '<h1>New user has been created!</h1>'
-        except IntegrityError:
-            db.session.rollback()
-
-
-        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
-    return render_template('signup.html', form=form)
-
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-
+# class LoginForm(FlaskForm):
+#     #username part of login
+#     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+#     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+#     remember = BooleanField('remember me')
+#
+# class RegisterForm(FlaskForm):
+#     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+#     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+#     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+#
+# class User(db.Model):
+#     id = db.Column('id', db.Integer, primary_key=True)
+#     username = db.Column(db.String(15), unique=True)
+#     email = db.Column(db.String(50), unique=True)
+#     password = db.Column(db.String(80))
+#
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+#
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     form = LoginForm()
+#     #checks to see if form has been submitted
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.username.data).first()
+#         if user:
+#             #if user logged in correctly, redirect to the dashboard
+#             if user.password == form.password.data:
+#                 return redirect(url_for('dashboard'))
+#         #if user doesn't exist or login incorrect
+#         return '<h1> Invalid username or password </h1>'
+#         #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+#     return render_template('login.html', form = form)
+#
+# @app.route('/signup', methods=['POST', 'GET'])
+# def signup():
+#     form = RegisterForm()
+#     #allow new user to be created
+#     if form.validate_on_submit():
+#         new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+#         db.session.add(new_user)
+#         try:
+#             db.session.commit()
+#             return '<h1>New user has been created!</h1>'
+#         except IntegrityError:
+#             db.session.rollback()
+#
+#
+#         #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+#     return render_template('signup.html', form=form)
+# 
+# @app.route('/dashboard')
+# def dashboard():
+#     return render_template('dashboard.html')
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 
 
