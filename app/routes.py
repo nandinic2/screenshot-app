3b818819046fcd30cb21c0231fc40efffcaae2c3
nandinic2 <2020nchakravorty@dwight.edu>
2019-11-23 11:44:09 -0500

    final

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,7 @@
 from app import app
-from flask import Flask, request, send_file, url_for, render_template, redirect
+from flask import Flask, request, send_file, url_for, render_template, redirect, session, g
 from flask_bootstrap import Bootstrap
 from app.models import model
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
-from flask_sqlalchemy  import SQLAlchemy
-from sqlalchemy.exc import IntegrityError
 import pyautogui
 import time
 
@@ -14,124 +9,130 @@ from flask_pymongo import PyMongo
 
 app.config['SECRET_KEY'] = 'sanskriti2012'
 app.config['MONGO_URI'] = 'mongodb+srv://admin:teesi5o1gOex2h5G@cluster0-n5tzk.mongodb.net/pictures?retryWrites=true&w=majority'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/2020nchakravorty/Desktop/screenshottest.db'
+app.secret_key= 'b\x1f\x01\xc2\xa9\xd9\x02Cm\x85\x86'
 
-
-db = SQLAlchemy(app)
 
 mongo = PyMongo(app)
 Bootstrap(app)
 
-#the login form
-# class LoginForm(FlaskForm):
-#     #username part of login
-#     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-#     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-#     remember = BooleanField('remember me')
-#
-# class RegisterForm(FlaskForm):
-#     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-#     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-#     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-#
-# class User(db.Model):
-#     id = db.Column('id', db.Integer, primary_key=True)
-#     username = db.Column(db.String(15), unique=True)
-#     email = db.Column(db.String(50), unique=True)
-#     password = db.Column(db.String(80))
-#
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-#
-# @app.route('/login', methods=['POST', 'GET'])
-# def login():
-#     form = LoginForm()
-#     #checks to see if form has been submitted
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(username=form.username.data).first()
-#         if user:
-#             #if user logged in correctly, redirect to the dashboard
-#             if user.password == form.password.data:
-#                 return redirect(url_for('dashboard'))
-#         #if user doesn't exist or login incorrect
-#         return '<h1> Invalid username or password </h1>'
-#         #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
-#     return render_template('login.html', form = form)
-#
-# @app.route('/signup', methods=['POST', 'GET'])
-# def signup():
-#     form = RegisterForm()
-#     #allow new user to be created
-#     if form.validate_on_submit():
-#         new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
-#         db.session.add(new_user)
-#         try:
-#             db.session.commit()
-#             return '<h1>New user has been created!</h1>'
-#         except IntegrityError:
-#             db.session.rollback()
-#
-#
-#         #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
-#     return render_template('signup.html', form=form)
-# 
-# @app.route('/dashboard')
-# def dashboard():
-#     return render_template('dashboard.html')
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+#sign up
+@app.route('/signup', methods=['POST', 'GET' ])
+def signup():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name': request.form['username']})
+
+        if existing_user is None:
+            users.insert({'name':request.form['username'], 'password': request.form['password']})
+            session['username'] = request.form
+            return redirect(url_for('index'))
+
+        return 'That username already exists!'
+    return render_template('signup.html')
+
+#log in
+@app.route('/login', methods=['POST'])
+def login():
+    #connects to users database
+    users = mongo.db.users
+    #queries through database to find saved user with the same username as inputted in the form.
+    login_user = users.find_one({'name': request.form['username']})
+    #if such username exists in the database
+    if login_user:
+        if request.form['password'] == login_user['password']:
+            #start the user's session
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+    #else
+    return 'Invalid username/password combination'
+
+#Log out
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
 
 
 
-@app.route('/input', methods=["GET"])
-def input():
-    return render_template('input.html')
-
-@app.route('/create',  methods = ["POST"])
+@app.route('/create',  methods = ['POST'])
 def create():
-    if "screenshot" in request.files:
-        screenshot = request.files["screenshot"]
-        mongo.save_file(screenshot.filename, screenshot)
-        mongo.db.pictures.insert({'username': request.form.get('username'),'screenshot':screenshot.filename})
-    return '''<a href='index'>Enter new screenshot</a>
-    <a href='/profile'>View past screenshots</a>'''
+    #retrieve screenshot posted from input template.
+    screenshot = request.files['screenshot']
+    #retrieves user’s username from the session.
+    username = session.get('username')
+    #prepares screenshot to be added to Mongo “pictures” database.
+    mongo.save_file(screenshot.filename, screenshot)
+    username = session.get('username')
+    #retrieve name and category of screenshot posted from input template.
+    category = request.form['category']
+    #makes category input uppercase
+    category = category.upper()
+    file_name = request.form['file_name']
+    #adds username, picture, category and file name to Mongo “pictures” database.
+    mongo.db.pictures.insert({'username': username,'screenshot':screenshot.filename,'category': category,'file_name': file_name})
+    return redirect(url_for('pictures'))
+
+@app.route('/searched', methods = ['POST'])
+def searched():
+    search = request.form['search']
+    #makes search input uppercase
+    search = search.upper()
+    #retrieves username from session
+    username = session.get('username')
+    #queries database to find all pictures under the entered category
+    searched_terms = mongo.db.pictures.find({'username': username, 'category':search})
+    return render_template('searched.html', searched_terms = searched_terms)
+
 
 @app.route('/file/<filename>')
 def file(filename):
     return mongo.send_file(filename)
 
-@app.route('/profile/<username>')
-def profile(username):
-    pictures = list(mongo.db.pictures.find({}))
-    print(pictures)
-    return render_template('profile.html', pictures = pictures, username = username)
+@app.route('/pictures')
+def pictures():
+    #check if session running
+    if session.get('username', None) is not None:
+        username = session.get('username')
+        #query database using username
+        pictures = list(mongo.db.pictures.find({'username':username}))
+        #render pictures template and send all pictures found to template
+        return render_template('pictures.html', pictures = pictures)
+    else:
+        return render_template('pictures.html')
 
-@app.route('/screenshot')
-def screenshot():
-    pictures = list(events.find({}))
-    print("3")
-    time.sleep(1)
-    print("2")
-    time.sleep(1)
-    print("1")
-    img = pyautogui.screenshot()
-    time.sleep(2)
-    return send_file(img)
+@app.route('/input', methods=["GET"])
+def input():
+    return render_template('input.html')
 
+#deleting
 @app.route('/deleteAll', methods = ['Get','Post'])
 def deleteAll():
-
+    users = mongo.db.users
+    #connects to the database
+    collection = mongo.db.pictures
     userdata = dict(request.form)
-    #names password collected from form as password
-    password = userdata['password']
-    if password == "clearit!":
-        #connects to the database
-        collection = mongo.db.pictures
-        #deletes all entries in the database
-        collection.delete_many({})
-        #loads deleteAll.html
-        return render_template("deleteAll.html", password = password)
+    if userdata['name'] == '':
+        #retrieve username from session
+        username = session.get('username')
+        password = userdata['password']
+        #names password collected from form as password
+        user = users.find_one({'name': username})
+        passedword = user['password']
+        if password == passedword:
+            #deletes all entries in the database
+            collection.delete_many({'username': username})
+            #loads deleteAll.html
+            return render_template('deleteAll.html')
+    #if no password inputted
+    elif userdata['password'] == '':
+        #userdata only equal to name of file
+        userdata = userdata['name']
+        #delete specific file
+        collection.delete_many({'file_name':userdata})
+        return render_template('deleteAll.html')
     else:
-        return "incorrect password"
+        return 'Try again'
